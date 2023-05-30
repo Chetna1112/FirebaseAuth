a0a39028c989dd90c9e8e84070fbf1377c1e7a3d
Nagesh Kumar Mishra <canmishra8@gmail.com>
2023-05-30 16:13:46 +0530

    user authentication

---
+++ app/src/main/java/com/example/firebaseauth/LoginActivity.kt
@@ -0,0 +1,31 @@
+package com.example.firebaseauth
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.firebaseauth.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.title="Login"
+        binding.registerTV.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+            finish()
+        }
+        binding.loginBtn.setOnClickListener {
+            val email = binding.emaillogin.text.toString()
+            val password=binding.passwordlogin.text.toString()
+            if(email.isNotEmpty()&&password.isNotEmpty())
+                MainActivity.auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if(it.isSuccessful){
+                        startActivity(Intent(this,MainActivity::class.java))
+                        finish()
+                    }
+                }.addOnFailureListener { Toast.makeText(this,it.localizedMessage, Toast.LENGTH_LONG).show() }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/firebaseauth/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.firebaseauth
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.firebaseauth.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    companion object{
+        lateinit var auth:FirebaseAuth
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth=FirebaseAuth.getInstance()
+        if(auth.currentUser==null)
+        {
+            startActivity(Intent(this,RegisterActivity::class.java))
+            finish()
+
+        }
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.signIn.setOnClickListener{
+            startActivity(Intent(this,RegisterActivity::class.java))
+            finish()
+        }
+        binding.signOut.setOnClickListener {
+            auth.signOut()
+            binding.userDetails.text=updateData()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.userDetails.text=updateData()
+    }
+    private fun updateData():String{
+        return "Email: ${auth.currentUser?.email}"
+    }
+}
